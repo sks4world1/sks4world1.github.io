1f26520cf61c5dfea885a7cc7dae79fc034ad470
sks4world1 <57736377+sks4world1@users.noreply.github.com>
2023-12-25 22:25:04 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,15 +1,31 @@
 <script>
     // Your JavaScript code
-    document.addEventListener('DOMContentLoaded', function() {
-      const hoverText = document.getElementById('hover-text');
-      const displayText = document.getElementById('display-text');
+    const hoverText = document.getElementById('hover-text');
+    const displayText = document.getElementById('display-text');
 
-      hoverText.addEventListener('mouseover', function() {
-        displayText.style.display = 'block';
-      });
-
-      hoverText.addEventListener('mouseout', function() {
-        displayText.style.display = 'none';
-      });
+    hoverText.addEventListener('mouseover', function() {
+      displayText.style.display = 'block';
     });
+
+    hoverText.addEventListener('mouseout', function() {
+      displayText.style.display = 'none';
+    });
+
+
+
+// JavaScript for FAQ accordion functionality
+const accordions = document.getElementsByClassName('accordion');
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+    const panel = this.nextElementSibling;
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
+    } else {
+      panel.style.display = 'block';
+    }
+  });
+}
+
   </script>
